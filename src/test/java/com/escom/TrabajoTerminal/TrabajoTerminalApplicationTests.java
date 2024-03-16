f79f6437350d70008659f3aa9aa3555f76b8e8be
PsyDuck. <david-balta@hotmail.com>
2024-03-16 15:41:37 -0600

    <david> Creacion del proyecto

---
+++ src/test/java/com/escom/TrabajoTerminal/TrabajoTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escom.TrabajoTerminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
